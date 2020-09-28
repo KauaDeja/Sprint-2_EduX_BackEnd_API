b7a76064371bfdba62dbf6b7f340493595282242
Gabriel <gabriel.gaspargbg@gmail.com>
2020-09-28 18:02:17 -0300

    Initial Commit (Objetivo e ObjetivoAluno)

---
+++ Projeto_EduXSprint2/Interfaces/IObjetivoAlunoRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_EduXSprint2.Interfaces
+{
+    interface IObjetivoAlunoRepository
+    {
+    }
+}
