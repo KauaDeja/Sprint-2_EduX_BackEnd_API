7fce7922d3aa103e838da1129faec49ea5fe321e
Dejailson Silva <kauakarate@gmail.com>
2020-10-01 12:12:59 -0300

    arrumando

--- Projeto_EduXSprint2/DQL_Edux001.sql
+++
@@ -1,4 +0,0 @@
-
-	SELECT AlunoTurma.Matricula, Turma.Descricao , Usuario.Nome FROM AlunoTurma
-			INNER JOIN Turma ON AlunoTurma.IdTurma = Turma.IdTurma
-			INNER JOIN Usuario ON Usuario.IdUsuario = AlunoTurma.IdUsuario
