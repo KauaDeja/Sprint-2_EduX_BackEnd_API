ab76090cf5bbb0d557f617243bf353f7871664ed
Dejailson Silva <kauakarate@gmail.com>
2020-10-01 12:04:14 -0300

    arquivos certos atualizados

---
+++ Projeto_EduXSprint2/Projeto_EduXSprint2/Domains/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto_EduXSprint2.Domains
+{
+    public partial class Categoria
+    {
+        public Categoria()
+        {
+            Objetivo = new HashSet<Objetivo>();
+        }
+
+        public Guid IdCategoria { get; set; }
+        public string Tipo { get; set; }
+
+        public virtual ICollection<Objetivo> Objetivo { get; set; }
+    }
+}
